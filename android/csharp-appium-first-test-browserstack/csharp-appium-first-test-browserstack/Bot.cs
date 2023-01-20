8ab16c00cb62ba9be7973cee9a09b2369ca25bd6
kingly <ratanaly92@gmail.com>
2023-01-21 00:16:50 +0700

    update

--- android/csharp-appium-first-test-browserstack/csharp-appium-first-test-browserstack/Bot.cs
+++ android/csharp-appium-first-test-browserstack/csharp-appium-first-test-browserstack/Bot.cs
@@ -9,6 +9,7 @@ namespace csharp_appium_first_test_browserstack
 {
     public class Bot
     {
+        private int count = 1;
         public void Start()
         {
             AndroidDriver<AndroidElement> driver = null;
@@ -22,8 +23,8 @@ namespace csharp_appium_first_test_browserstack
             caps.AddAdditionalCapability("app", "bs://79230c428c3c63a34bf434afa36c565c8f51782d");
 
             // Specify device and os_version
-            caps.AddAdditionalCapability("device", "Samsung Galaxy M52");
-            caps.AddAdditionalCapability("os_version", "11.0");
+            caps.AddAdditionalCapability("device", "Xiaomi Redmi Note 9");
+            caps.AddAdditionalCapability("os_version", "10.0");
 
             // Specify the platform name
             caps.PlatformName = "Android";
@@ -34,52 +35,79 @@ namespace csharp_appium_first_test_browserstack
             caps.AddAdditionalCapability("name", "BOT APP");
             driver = new AndroidDriver<AndroidElement>(new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
 
-            int count = 10;
-            while (count > 0)
+            while (true)
             {
-                System.Threading.Thread.Sleep(5000);
-                try
-                {
-                    AndroidElement searchElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(10))
-                                              .Until(SeleniumExtras.WaitHelpers.ExpectedConditions
-                                              .ElementToBeClickable(MobileBy.Id("com.cxwv.unblockme:id/rewarded")));
-                    if (searchElement != null && searchElement.Text == "READY SHOW REWARDED VIDEO")
-                    {
-                        Console.WriteLine("name : " + searchElement.Text);
-                        searchElement.Click();
+                System.Threading.Thread.Sleep(2000);
 
-                        System.Threading.Thread.Sleep(31000);
-                        
-
-                        AndroidElement close = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(10))
-                                             .Until(SeleniumExtras.WaitHelpers.ExpectedConditions
-                                             .ElementToBeClickable(MobileBy.XPath("	/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.widget.Button")));
-                        bool isSearchClose = true;
-                        while(isSearchClose)
-                        {
-                            if(close != null && close.Text == "Close")
-                            {
-                                Console.WriteLine("back : <=");
-                                close.Click();
-                                break;
-                            }
-                            else
-                            {
-                                System.Threading.Thread.Sleep(2000);
-                            }
-                            
-                        }
-                        //driver.PressKeyCode(AndroidKeyCode.Back);
-                    }
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine("error : " + ex.Message);
-                }
+                SearchShow(driver);
+                SearchClose(driver);
               
             }
 
             driver.Quit();
+        }
+
+        public void SearchShow(AndroidDriver<AndroidElement> driver)
+        {
+            try
+            {
+
+                AndroidElement searchElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+                                  .Until
+                                  (SeleniumExtras.WaitHelpers.ExpectedConditions
+                                  .ElementToBeClickable(MobileBy.Id("com.cxwv.unblockme:id/rewarded")));
+                if (searchElement != null && searchElement.Text == "READY SHOW REWARDED VIDEO")
+                {
+                    Console.WriteLine("name : " + searchElement.Text);
+                    searchElement.Click();
+                    System.Threading.Thread.Sleep(31000);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error : " + ex.Message);
+                switch (count)
+                {
+                    case 1: SearchShow(driver); break;
+                    case 2: SearchClose(driver); break;
+                }
+                count++;
+                if (count >= 3)
+                    count = 1;
+            }
+
+        }
+
+        public void SearchClose(AndroidDriver<AndroidElement> driver)
+        {
+
+            try
+            {
+
+                AndroidElement close = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+                         .Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+                         .ElementToBeClickable(MobileBy.XPath("	/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.widget.Button")));
+
+                if (close != null && close.Text == "Close")
+                {
+                    Console.WriteLine("back : <=");
+                    close.Click();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error : " + ex.Message);
+                switch (count)
+                {
+                    case 1: SearchShow(driver); break;
+                    case 2: SearchClose(driver); break;
+                }
+                count++;
+                if (count >= 3)
+                    count = 1;
+            }
         }
     }
 }
